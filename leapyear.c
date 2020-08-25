fd68fd1fa3d0aa8738ed2379a420edda2d957dc4
Mithun Goswami <goswamimithun2019@yahoo.com>
2020-08-25 21:35:23 +0530

    first commit

---
+++ leapyear.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+
+void main() {
+
+	char ch;
+	
+     do
+     {
+     int year;
+     printf("Enter year \n");
+     scanf("%d", &year);
+     if(year<1000 || year >9999){
+     	printf("Invalide entry ! \n");
+     	goto abc;
+		 }
+     
+	 else{
+	 	if(year%400==0)
+	 	printf("This is leap year \n");
+	 	else if(year%100==0)
+	 	printf("This is not leap year \n");
+	 	else if(year%4==0)
+	 	printf("This is leap year \n");
+	 	else
+	 	printf("This is not leap year \n");
+	 }
+	 abc:
+	 printf("Do you want to continue press y :");
+    scanf(" %c", &ch);
+}while(ch == 'Y' || ch == 'y');
+}
